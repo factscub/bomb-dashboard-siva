5e06041ae595d9ef49254d46e402c244f7323570
factscub <factscub@gmail.com>
2023-01-14 02:15:18 +0530

    added paramter to function

--- src/hooks/useHarvest.ts
+++ src/hooks/useHarvest.ts
@@ -7,10 +7,10 @@ const useHarvest = (bank: Bank) => {
   const bombFinance = useBombFinance();
   const handleTransactionReceipt = useHandleTransactionReceipt();
 
-  const handleReward = useCallback(() => {
+  const handleReward = useCallback((BANK) => {
     handleTransactionReceipt(
-      bombFinance.harvest(bank.contract, bank.poolId),
-      `Claim ${bank.earnTokenName} from ${bank.contract}`,
+      bombFinance.harvest(bank ? bank.contract : BANK.contract,bank? bank.poolId : BANK.poolId),
+      `Claim ${bank ? bank.earnTokenName : BANK.earnTokenName} from ${bank ? bank.contract : BANK.contract }`,
     );
   }, [bank, bombFinance, handleTransactionReceipt]);
 
